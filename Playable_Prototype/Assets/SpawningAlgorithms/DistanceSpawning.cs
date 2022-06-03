ba5ea30291a0dcb3855030d3cf242b09f75d8cd7
brayww <birdenthebirdman@gmail.com>
2022-06-02 19:36:36 -0700

    spawning with distance detection

---
+++ Playable_Prototype/Assets/SpawningAlgorithms/DistanceSpawning.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DistanceSpawning : MonoBehaviour
+{
+    public GameObject[] players;
+    public GameObject[] spawns;
+    public int distance;
+
+    void Awake()
+    {
+        players = GameObject.FindGameObjectsWithTag("Player");
+        spawns = GameObject.FindGameObjectsWithTag("SpawnPoint");
+        foreach (var player in players)
+        {
+            Spawn(player);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Spawn(GameObject player)
+    {
+        List<GameObject> filtered = new List<GameObject>(spawns);
+        foreach(GameObject spawn in spawns)
+        {
+            foreach(GameObject p in players)
+            {
+                if(Vector3.Distance(spawn.transform.position, p.transform.position) > distance)
+                {
+                    filtered.Remove(spawn);
+                    break;
+                }
+            }
+        }
+
+        player.transform.position = filtered[Random.Range(0, filtered.Count)].transform.position;
+    }
+}
